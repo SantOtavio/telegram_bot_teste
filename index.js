7cff1f4fa63acfc70b25727e93043e0484a9073d
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 11:22:04 -0300

    a

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+// Função para enviar uma mensagem de texto
+async function sendMessage(token, chatId, messageText) {
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    try {
+        const response = await axios.post(url, {
+            chat_id: chatId,
+            text: messageText
+        });
+
+        if (response.status === 200) {
+            console.log('Mensagem enviada com sucesso!');
+        } else {
+            console.log('Erro ao enviar a mensagem:', response.data);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar a mensagem:', error.message);
+    }
+}
+
+// Rota para enviar a mensagem
+app.get('/mensagem', (req, res) => {
+    const token = '6053515990:AAG0G781j1g7PAYUx6t0nmJljV5RcMLSQXc';
+    const chatId = '1624643673';
+    const messageText = 'Temperatura: teste';
+
+    sendMessage(token, chatId, messageText);
+
+    res.send('Mensagem enviada!');
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
